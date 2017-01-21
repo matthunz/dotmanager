dbce3d246947af52e69cb4b9d55339071cd9afb1
matthunz <matthunz2@gmail.com>
2017-01-20 23:21:42 -0500

    Add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Dotmanager")
+        .arg(Arg::with_name("config")
+             .short("c")
+             .long("config")
+             .help("Sets a custom config file")
+             .takes_value(true))
+        .get_matches();
+
+    println!("{}", matches.value_of("config").unwrap_or("none"));
 }
